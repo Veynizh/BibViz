0faf8489cbe80e2b9ca5f4d90fad75dfe4d96f49
Veynizh <55839966+Veynizh@users.noreply.github.com>
2020-06-25 16:52:30 +0700

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,640 @@
+import React from "react";
+import {
+  TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Card,
+  CardHeader,
+  CardContent,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  List,
+  ListItem,
+  ListItemText,
+  Button,
+} from "@material-ui/core";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Box from "@material-ui/core/utils";
+import readXlsxFile from "read-excel-file";
+// import Papa from 'papaparse'
+// import fs from 'fs'
+// const fs = require('fs-extra');
+// import { readFile, readFileSync } from 'fs';
+// import d3 from 'react-d3-library';
+import * as d3 from "d3";
+import firebase from "./config/firebase";
+import { Chart } from "react-google-charts";
+
+const db_main_table = firebase
+  .firestore()
+  .collection("AuthorXContentXReserveXYears");
+const db_keys_table = firebase.firestore().collection("Keys");
+
+// const AUTHOR_FIELD = require("./data/author_field.json");
+
+export default class Home extends React.Component {
+  state = {
+    search: "",
+    mode: "Author-Field",
+    result: [],
+    history: [
+      // "Author-Field - ThanasartThanasartThanasartThanasartThanasartThanasartThanasart",
+      // "Keyword-ID, Title - Auto",
+      // "Author-Author - KMITL",
+    ],
+    chartData: [],
+    chartTitle: "",
+    chartTitleX: "",
+    chartTitleY: "",
+    searchOption: [],
+  };
+  componentDidMount = async () => {
+    const authors = await this.getAuthors();
+    const authorsName = authors["Name"];
+    this.setState({ searchOption: authorsName });
+  };
+  getAuthors = () =>
+    new Promise((resolve, reject) => {
+      db_keys_table
+        .doc("Author")
+        .get()
+        .then((snapshot) => {
+          // console.log(snapshot);
+          // console.log(snapshot.empty);
+          resolve(snapshot.data());
+        });
+    });
+  onChangeTextOption = (event, value) => {
+    this.setState({ search: value });
+  };
+  onChangeText = (event) => {
+    const { value } = event.target;
+    this.setState({ search: value });
+  };
+  handleChangeMode = (event) => {
+    const { value } = event.target;
+    this.resetChart(() => {
+      this.setState({ mode: value });
+    });
+  };
+  resetChart = (callback = () => {}) =>
+    this.setState(
+      {
+        search: "",
+        chartData: [],
+        chartTitle: "",
+        chartTitleX: "",
+        chartTitleY: "",
+      },
+      callback
+    );
+  onClickHistory = (mode, search) => {
+    this.setState({ mode, search }, () => {
+      this.onSubmit();
+    });
+  };
+  saveHistory = (search) => {
+    const { mode, history } = this.state;
+    history.push([mode, search].join(" - "));
+    this.setState({ history });
+  };
+  onSubmit = () => {
+    const { mode } = this.state;
+    if (mode == "Author-Field") {
+      console.log("submit author-feild");
+      this.processAuthorField();
+    } else if (mode == "Keyword-ID, Title") {
+      this.processKeyword();
+    } else {
+    }
+  };
+  processAuthorField = async () => {
+    const { search } = this.state;
+    this.saveHistory(search);
+
+    console.log("in process author-field");
+
+    const fields = await this.getFieldByAuthor(search);
+    const chartData = fields.map((obj) => [
+      obj["freq_reserve"],
+      obj["freq_genre"],
+      "o",
+      obj["fieldName"],
+    ]);
+    chartData.unshift([
+      "",
+      "",
+      {
+        role: "annotation",
+        type: "string",
+      },
+      {
+        role: "annotationText",
+        type: "string",
+      },
+    ]);
+
+    console.log("fnish cal chart daata");
+
+    console.log(chartData);
+
+    const chartTitle = search + " Fields";
+    const chartTitleX = "Frequency Reserve";
+    const chartTitleY = "Frequency Genre";
+
+    this.setState({ chartData, chartTitle, chartTitleX, chartTitleY });
+  };
+  getFieldByAuthor = (author) =>
+    new Promise((resolve, reject) => {
+      console.log("det f", author);
+      const marc_tags = [
+        "marc_tag.100",
+        "marc_tag.110",
+        "marc_tag.700",
+        "marc_tag.710",
+      ];
+
+      const promises = marc_tags.map((marc_tag) =>
+        db_main_table
+          .where(marc_tag, "array-contains", author) // ==
+          .get()
+      );
+
+      Promise.all(promises)
+        .then((snapshots) => {
+          console.log(snapshots);
+          if (snapshots.every((snapshot) => snapshot.empty)) {
+            console.log("No matching documents.");
+            return;
+          }
+
+          const result = {};
+          const doc_objs = {};
+
+          snapshots.forEach((snapshot) => {
+            snapshot.forEach((doc) => {
+              doc_objs[doc.id] = doc;
+            });
+          });
+
+          Object.values(doc_objs).forEach((doc) => {
+            // console.log(doc.id, "=>", doc.data());
+
+            const data = doc.data();
+
+            const fieldNames = data["marc"]["650"];
+            const bookName =
+              "marc" in data && "245" in data["marc"]
+                ? data["marc"]["245"][0]
+                : "-";
+
+            const sumCount =
+              doc.data()["checkout"] +
+              doc.data()["renew"] +
+              doc.data()["internal"];
+
+            fieldNames.forEach((fieldName) => {
+              if (!Object.keys(result).includes(fieldName)) {
+                result[fieldName] = {
+                  detail: [],
+                  freq_genre: 0,
+                  freq_reserve: 0,
+                };
+              }
+
+              result[fieldName]["detail"].push({
+                bookName,
+                freq: sumCount,
+              });
+              result[fieldName]["freq_genre"] += 1;
+              result[fieldName]["freq_reserve"] += sumCount;
+            });
+          });
+
+          const resultWithTop = Object.keys(result).map((fieldName) => {
+            const field = result[fieldName];
+            return {
+              book_top_three: field["detail"]
+                .sort((a, b) => a.freq - b.freq)
+                .slice(0, 3),
+              freq_genre: field["freq_genre"],
+              freq_reserve: field["freq_reserve"],
+              fieldName,
+            };
+          });
+          resolve(resultWithTop);
+        })
+        .catch((err) => {
+          console.log("Error getting documents", err);
+        }); // สำนักงานคณะกรรมการสิ่งแวดล้อมแห่งชาติ
+    });
+  processKeyword = async () => {
+    const { search } = this.state;
+    // const search = "frequency"; // telephone
+    this.saveHistory(search);
+
+    const field_author = await this.getByKeyword(search);
+    const chartData = [
+      ["Author", "Parent", "number of book", "frequency of reserve"],
+      [search, null, 0, 0],
+    ];
+    Object.keys(field_author).forEach((field) => {
+      chartData.push([field, search, 0, 0]);
+      Object.keys(field_author[field]).forEach((author) => {
+        const { n_book, freq_reserve } = field_author[field][author];
+        chartData.push([
+          { v: author + field, f: author },
+          field,
+          n_book,
+          freq_reserve,
+        ]);
+      });
+    });
+
+    console.log(chartData);
+
+    this.setState({ chartData });
+  };
+  getByKeyword = (keyword) =>
+    new Promise((resolve, reject) => {
+      const queryText = keyword;
+      console.log("start");
+      db_main_table
+        .where("keywords", "array-contains", queryText)
+        .get()
+        .then((snapshot) => {
+          console.log(snapshot);
+          if (snapshot.empty) {
+            console.log("No matching documents.");
+            return;
+          }
+          const field_author = {};
+          snapshot.docs.forEach((doc) => {
+            const data = doc.data();
+            console.log(data);
+            if (!("marc" in data && "650" in data["marc"])) {
+              return;
+            }
+            const fields = [].concat(...Object.values(data["marc"]["650"]));
+            const authors = [].concat(...Object.values(data["marc_tag"]));
+            fields.forEach((field) => {
+              if (!(field in field_author)) {
+                field_author[field] = {};
+              }
+              authors.forEach((author) => {
+                if (!(author in field_author[field])) {
+                  field_author[field][author] = {
+                    n_book: 0,
+                    freq_reserve: 0,
+                  };
+                }
+                const sumCount =
+                  data["checkout"] + data["renew"] + data["internal"];
+                field_author[field][author]["n_book"] += 1;
+                field_author[field][author]["freq_reserve"] += sumCount;
+              });
+            });
+            resolve(field_author);
+          });
+        });
+    });
+  testDataTree = () => {
+    // var test = [
+    //   [
+    //     "Location",
+    //     "Parent",
+    //     "Market trade volume (size)",
+    //     "Market increase/decrease (color)",
+    //   ],
+    //   ["ABN", null, 0, 0],
+    // ];
+
+    // for (var i = 1; i <= 50; i++) {
+    //   const p = i <= 30 ? "ABN" : i - 30;
+    //   const f = i == 31 ? 1 : i == 32 ? 500 : 1;
+    //   const c = i % 10;
+    //   test.push([i.toString(), p.toString(), f, c]);
+    // }
+    // console.log(test);
+    const test = [
+      ["Person", "Fruit", "Size", "Color"],
+      ["Global", null, 0, 0],
+      ["Bananas", "Global", 0, 0],
+      [{ v: "Rick0", f: "Rick" }, "Bananas", 100, 0],
+      [{ v: "Anne0", f: "Anne" }, "Bananas", 40, 0],
+      [{ v: "Peter0", f: "Peter" }, "Bananas", 5, 0],
+      ["Apples", "Global", 0, 0],
+      [{ v: "Anne1", f: "Anne" }, "Apples", 20, 2],
+      [{ v: "Peter1", f: "Peter" }, "Apples", 20, 2],
+      [{ v: "Rick1", f: "Rick" }, "Apples", 15, 2],
+      ["Oranges", "Global", 0, 0],
+      [{ v: "Rick2", f: "Rick" }, "Oranges", 20, 1],
+      [{ v: "Peter2", f: "Peter" }, "Oranges", 20, 1],
+      [{ v: "Anne2", f: "Anne" }, "Oranges", 10, 1],
+      ["Susanne", "Global", 10, null],
+    ];
+    return test;
+  };
+  renderSearchBar = () => {
+    const { search, searchOption, mode } = this.state;
+    return mode == "Author-Field" ? (
+      <Autocomplete
+        id="txt-search"
+        debug
+        value={search}
+        options={searchOption}
+        onChange={this.onChangeTextOption}
+        renderInput={(params) => <TextField {...params} label="Search" />}
+      />
+    ) : mode == "Keyword-ID, Title" ? (
+      <TextField
+        id="txt-search"
+        value={search}
+        onChange={this.onChangeText}
+        label="Search"
+      />
+    ) : (
+      // Author-Author
+      <TextField
+        id="txt-search"
+        value={search}
+        onChange={this.onChangeText}
+        label="Search"
+      />
+    );
+  };
+  renderChart = () => {
+    const {
+      mode,
+      chartData,
+      chartTitle,
+      chartTitleX,
+      chartTitleY,
+    } = this.state;
+    return mode == "Author-Field" ? (
+      <Chart
+        id="chart"
+        // width={"600px"}
+        height={"100vh"}
+        chartType="ScatterChart"
+        loader={<div>Loading Chart</div>}
+        data={chartData}
+        options={{
+          title: chartTitle,
+          hAxis: {
+            title: chartTitleX,
+            maxValue: Math.max(...chartData.slice(1).map((d) => d[0])) + 1,
+          },
+          vAxis: {
+            title: chartTitleY,
+            maxValue: Math.max(...chartData.slice(1).map((d) => d[1])) + 1,
+            minValue: Math.min(...chartData.slice(1).map((d) => d[1])) - 1,
+          },
+          explorer: {
+            axis: "horizontal",
+            keepInBounds: false,
+            maxZoomIn: 1000,
+            zoomDelta: 0.9,
+          },
+          legend: "none",
+        }}
+        rootProps={{ "data-testid": "1" }}
+        chartEvents={[
+          {
+            eventName: "select",
+            callback: ({ chartWrapper }) => {
+              const chart = chartWrapper.getChart();
+              const selection = chart.getSelection();
+              if (selection.length === 1) {
+                const { search } = this.state;
+                const [selectedItem] = selection;
+                const dataTable = chartWrapper.getDataTable();
+                const { setAuthor, setField } = this.props;
+                setAuthor(search);
+                setField(chartData[selectedItem["row"] + 1][3]);
+                // console.log(this.props.history);
+                this.props.history.push("/table");
+              }
+              console.log(selection);
+            },
+          },
+        ]}
+      />
+    ) : mode == "Keyword-ID, Title" ? (
+      <Chart
+        // id="chart"
+        // width={"500px"}
+        // height={"300px"}
+        height={"100vh"}
+        chartType="TreeMap"
+        loader={<div>Loading Chart</div>}
+        data={chartData}
+        // data={this.testDataTree()}
+        options={{
+          minColor: "#f00",
+          midColor: "#ddd",
+          maxColor: "#0d0",
+          headerHeight: 15,
+          fontColor: "black",
+          showScale: true,
+        }}
+        rootProps={{ "data-testid": "1" }}
+        chartEvents={[
+          {
+            eventName: "select",
+            callback: ({ chartWrapper }) => {
+              const chart = chartWrapper.getChart();
+              const selection = chart.getSelection();
+              const [selectedItem] = selection;
+              const target = chartData[selectedItem["row"] + 1];
+              if (typeof target[0] === "object") {
+                const author = target[0]["f"];
+                const field = target[1];
+                const { setAuthor, setField } = this.props;
+                setAuthor(author);
+                setField(field);
+                this.props.history.push("/table");
+              }
+            },
+          },
+        ]}
+      />
+    ) : (
+      // Author-Author
+      <Chart
+        id="chart"
+        // width={"600px"}
+        height={"100vh"}
+        chartType="ScatterChart"
+        loader={<div>Loading Chart</div>}
+        data={chartData}
+        options={{
+          title: chartTitle,
+          hAxis: {
+            title: chartTitleX,
+            maxValue: Math.max(...chartData.slice(1).map((d) => d[0])) + 1,
+          },
+          vAxis: {
+            title: chartTitleY,
+            maxValue: Math.max(...chartData.slice(1).map((d) => d[1])) + 1,
+            minValue: Math.min(...chartData.slice(1).map((d) => d[1])) - 1,
+          },
+          explorer: {
+            axis: "horizontal",
+            keepInBounds: false,
+            maxZoomIn: 1000,
+            zoomDelta: 0.9,
+          },
+          legend: "none",
+        }}
+        rootProps={{ "data-testid": "1" }}
+        chartEvents={[
+          {
+            eventName: "select",
+            callback: ({ chartWrapper }) => {
+              const chart = chartWrapper.getChart();
+              const selection = chart.getSelection();
+              if (selection.length === 1) {
+                const { search } = this.state;
+                const [selectedItem] = selection;
+                const dataTable = chartWrapper.getDataTable();
+                const { setAuthor, setField } = this.props;
+                setAuthor(search);
+                setField(chartData[selectedItem["row"] + 1][3]);
+                console.log(this.props.history);
+                this.props.history.push("/table");
+              }
+              console.log(selection);
+            },
+          },
+        ]}
+      />
+    );
+  };
+  render() {
+    const { mode, result, history } = this.state;
+    return (
+      <div id="container">
+        <div id="left">
+          <Card id="mode-contrainer">
+            <CardHeader
+              // title="Mode"
+              subheader="Mode"
+            />
+            <CardContent id="delete-padding">
+              <RadioGroup
+                id="delete-padding"
+                value={mode}
+                onChange={this.handleChangeMode}
+              >
+                <FormControlLabel
+                  value="Author-Field"
+                  control={<Radio />}
+                  label="Author-Field"
+                />
+                <FormControlLabel
+                  value="Keyword-ID, Title"
+                  control={<Radio />}
+                  label="Keyword-ID, Title"
+                />
+                <FormControlLabel
+                  value="Author-Author"
+                  control={<Radio />}
+                  label="Author-Author"
+                />
+              </RadioGroup>
+            </CardContent>
+          </Card>
+          <Card id="history-contrainer">
+            <CardHeader
+              // title="Mode"
+              subheader="History"
+            />
+            <CardContent>
+              <List>
+                {history.map((item, index) => {
+                  const mode = item.split(" - ")[0];
+                  const search = item.split(" - ")[1];
+                  return (
+                    <ListItem key={index} button>
+                      <ListItemText
+                        onClick={() => this.onClickHistory(mode, search)}
+                        style={{ color: "red" }}
+                        primary={item}
+                      />
+                      {/* <ListItemText primary={search} /> */}
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </CardContent>
+          </Card>
+        </div>
+        <div id="center">{this.renderChart()}</div>
+        <div id="right">
+          {this.renderSearchBar()}
+          <div id="search-table">
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>No.</TableCell>
+                    <TableCell align="center">Title</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {result.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell align="center">{index}</TableCell>
+                      <TableCell align="left">{item}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+          <Button
+            variant="outlined"
+            id="submit-btn"
+            // onClick={() =>
+            //   this.getByKeyword("cc").then((r) => console.log("fini"))
+            // }
+            onClick={this.onSubmit}
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+// import rd3 from 'react-d3-library';
+// import node from './d3test';
+// const RD3Component = rd3.Component;
+
+// export default class my_First_React_D3_Library_Component extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {d3: ''}
+//   }
+
+//   componentDidMount() {
+//     this.setState({d3: node});
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <RD3Component data={this.state.d3} />
+//       </div>
+//     )
+//   }
+// };
---
+++ src/TablePage.js
@@ -0,0 +1,193 @@
+import React from "react";
+import {
+  TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core";
+import firebase from "./config/firebase";
+
+const db_main_table = firebase
+  .firestore()
+  .collection("AuthorXContentXReserveXYears");
+
+export default class TablePage extends React.Component {
+  state = {
+    table: [],
+  };
+  componentDidMount = async () => {
+    const { author, feild } = this.props;
+    console.log("AUTHOR:", author, "||", "FEILD:", feild);
+    const table = await this.getBookByAuthorAndFeild(author, feild);
+    // console.log("table", table);
+    this.setState({ table });
+  };
+  getBookByAuthorAndFeild = (author, feild) =>
+    new Promise((resolve, reject) => {
+      const marc_tags = ["100", "110", "700", "710"];
+
+      const dbWithFeild = db_main_table.where(
+        "marc.650",
+        "array-contains",
+        feild
+      );
+
+      const result = [];
+
+      dbWithFeild.get().then((snapshot) => {
+        console.log(snapshot);
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+          return;
+        }
+        // s.forEach((ss) => console.log(ss.data()));
+        snapshot.docs.forEach((doc) => {
+          const data = doc.data();
+          // console.log(data);
+          if (
+            marc_tags.some(
+              (marc_tag) =>
+                "marc_tag" in data &&
+                marc_tag in data["marc_tag"] &&
+                data["marc_tag"][marc_tag].includes(author)
+            )
+          ) {
+            console.log(data);
+
+            const bookName =
+              "marc" in data && "245" in data["marc"]
+                ? data["marc"]["245"][0]
+                : "-";
+            const MainPersonal = data["marc_tag"]["100"];
+            const MainCorp = data["marc_tag"]["110"];
+            const AddedPersonal = data["marc_tag"]["700"];
+            const AddedCorp = data["marc_tag"]["710"];
+            const Year = data["years"];
+
+            const sumCount =
+              data["checkout"] + data["renew"] + data["internal"];
+
+            result.push({
+              bookName,
+              sumCount,
+              MainPersonal,
+              MainCorp,
+              AddedPersonal,
+              AddedCorp,
+              Year,
+            });
+          }
+        });
+
+        result.sort((a, b) => b["sumCount"] - a["sumCount"]);
+
+        resolve(result);
+      });
+
+      // const promises = marc_tags.map((marc_tag) =>
+      //   dbWithFeild.where(marc_tag, "array-contains", author).get()
+      // );
+
+      // Promise.all(promises)
+      //   .then((snapshots) => {
+      //     console.log(snapshots.map((snapshot) => snapshot.empty));
+      //     if (snapshots.every((snapshot) => snapshot.empty)) {
+      //       console.log("No matching documents.");
+      //       return;
+      //     }
+
+      //     const result = [];
+      //     const doc_objs = {};
+
+      //     snapshots.forEach((snapshot) => {
+      //       snapshot.forEach((doc) => {
+      //         doc_objs[doc.id] = doc;
+      //       });
+      //     });
+
+      //     Object.values(doc_objs).forEach((doc) => {
+      //       const bookName = doc.data()["marc_tag"]["245"];
+      //       const MainPersonal = doc.data()["marc_tag"]["100"];
+      //       const MainCorp = doc.data()["marc_tag"]["110"];
+      //       const AddedPersonal = doc.data()["marc_tag"]["700"];
+      //       const AddedCorp = doc.data()["marc_tag"]["710"];
+
+      //       const sumCount =
+      //         doc.data()["checkout"] +
+      //         doc.data()["renew"] +
+      //         doc.data()["internal"];
+
+      //       result.push({
+      //         bookName,
+      //         sumCount,
+      //         MainPersonal,
+      //         MainCorp,
+      //         AddedPersonal,
+      //         AddedCorp,
+      //       });
+      //     });
+
+      // })
+      // .catch((err) => {
+      //   console.log("Error getting documents", err);
+      // });
+    });
+  defaultWithBlank = (txt) => (txt ? txt : "-");
+  render() {
+    const { table } = this.state;
+    return (
+      <div id="container">
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>No.</TableCell>
+                <TableCell align="center">Title</TableCell>
+                <TableCell align="center">Frequency Reserve</TableCell>
+                <TableCell align="center">Main Entry - Personal Name</TableCell>
+                <TableCell align="center">
+                  Main Entry - Corporate Name
+                </TableCell>
+                <TableCell align="center">
+                  Added Entry - Personal Name
+                </TableCell>
+                <TableCell align="center">
+                  Added Entry - Corporate Name
+                </TableCell>
+                <TableCell align="center">Year</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {table.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell align="center">{index + 1}</TableCell>
+                  <TableCell align="center">{row["bookName"]}</TableCell>
+                  <TableCell align="center">{row["sumCount"]}</TableCell>
+                  <TableCell align="center">
+                    {this.defaultWithBlank(row["MainPersonal"])}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.defaultWithBlank(row["MainCorp"])}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.defaultWithBlank(row["AddedPersonal"])}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.defaultWithBlank(row["AddedCorp"])}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.defaultWithBlank(row["Year"])}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  }
+}
